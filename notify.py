1d6391a19f62d419a78737c3cab8c47a9162861f
mrdoro <mrdoro@me.com>
2018-06-28 14:51:40 +0200

    Put phone number over Environment variables

--- notify.py
+++ notify.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 import urllib
+import os
 
 #Create as python 2.7 runtime
 
@@ -9,11 +10,14 @@ def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=1))
     client = boto3.client('sns')
 
+    phonenum = os.environ['Phone']
+    print(phonenum)
+
     Image = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
 
     response = client.publish(
-        #Provide correct phone number
-        PhoneNumber = '+48111222333',
+
+        PhoneNumber = phonenum,
         Message = 'A new file was uploaded: ' + Image
 
     )
